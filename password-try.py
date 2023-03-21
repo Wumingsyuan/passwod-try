00aba0e1d85f8c3c5b3d2f42cb5d532be2444b61
wumingsyuan <michael31715@gmail.com>
2023-03-21 23:02:29 +0800

    change to i >0

--- password-try.py
+++ password-try.py
@@ -4,10 +4,11 @@
 #最多輸入三次
 #如果正確 就印出'登入成功'
 #如果不正確 就印出 '密碼錯誤! 還有_次機會!'
+#(補) 當還有剩餘機會的時候就迴圈
 
 password = 'a123456'
 i = 3 #剩餘機會
-while True :
+while i > 0 :
     pwd = input('請輸入密碼') #不能用password，因為重複使用
     if pwd == password : #不要寫'a123456' 不好!
         print('登入成功')
@@ -15,6 +16,5 @@ while True :
     else :
         i = i - 1
         print('登入失敗,還有', i, '次機會')
-        if i ==0 :
-            break
+        
         
